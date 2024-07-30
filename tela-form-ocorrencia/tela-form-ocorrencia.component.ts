466c72ab92e6be5b38606a8400bec10a024bd7a8
Cassio Tavares das Silva <cassio.tavares4@gmail.com>
2024-07-29 21:55:50 -0400

    atualização do Formulario

--- tela-form-ocorrencia/tela-form-ocorrencia.component.ts
+++ tela-form-ocorrencia/tela-form-ocorrencia.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+interface Produto {
+  nome: string;
+  cliente: string;
+}
+
 @Component({
   selector: 'app-tela-form-ocorrencia',
   templateUrl: './tela-form-ocorrencia.component.html',
@@ -8,9 +13,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class TelaFormOcorrenciaComponent implements OnInit {
   ocorrenciaForm: FormGroup;
-  tiposOcorrencia = ['Tipo 1', 'Tipo 2', 'Tipo 3'];
+  tiposOcorrencia = [
+    'Falha de Injeção', 
+    'Rebarba', 
+    'Furo Obstruido',
+    'Contaminação',
+    'Dimencional Fora do Especificado',
+    'Embalagem fora do Padrão'
+  ];
   turnos = ['1°', '2°', '3°', 'Comercial'];
   statusProdutos = ['100% bloqueado', 'Em Análise', 'Scrap'];
+  ugbOptions = ['UGB1', 'UGB2', 'UGB3'];
+  produto: Produto | null = null;
 
   constructor(private fb: FormBuilder) {
     this.ocorrenciaForm = this.fb.group({
@@ -41,6 +55,30 @@ export class TelaFormOcorrenciaComponent implements OnInit {
     const file = event.target.files[0];
     this.ocorrenciaForm.patchValue({
       arquivo: file
+    });
+  }
+
+  onCodigoProdutoBlur(): void {
+    const codigoProduto = this.ocorrenciaForm.get('codigoProduto')?.value;
+    if (codigoProduto) {
+      this.fetchProdutoDetails(codigoProduto).then(produto => {
+        this.produto = produto;
+        this.ocorrenciaForm.patchValue({
+          cliente: produto ? produto.cliente : ''
+        });
+      });
+    }
+  }
+
+  fetchProdutoDetails(codigoProduto: string): Promise<Produto | null> {
+    const produtosMock: { [key: string]: Produto } = {
+      '123': { nome: 'Chassis Controle', cliente: 'SAMSUNG' },
+      '321': { nome: 'Coroa Bomba de Combustivel', cliente: 'HONDA' }
+    };
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(produtosMock[codigoProduto] || null);
+      }, 500);
     });
   }
 }
